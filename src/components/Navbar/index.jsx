7ce7566ac60381080ffec47368eda6830029b449
Pilli Asha Preethi <p.ashapreethi@gmail.com>
2023-12-13 21:24:59 +0530

    fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { Link } from "react-router-dom";
 import {  useAuth0 } from "@auth0/auth0-react";
