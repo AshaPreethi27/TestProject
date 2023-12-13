5e5e789b4cd721f7ff30fdb35d0b9ca2c160e259
Pilli Asha Preethi <p.ashapreethi@gmail.com>
2023-12-13 15:32:02 +0530

    issue

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -54,7 +54,7 @@ function Navbar() {
       <div className="navbar-profile">
         <p className="person-name">{user?.name}</p>
         <p>{user?.email}</p>
-        <p>{user}</p>
+        {isAuthenticated ? console.log(user):""}
         <img
           src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
           alt="pro-pic"
