e10499bcfe55a908026da1fcbe01848a62ae3644
Pilli Asha Preethi <p.ashapreethi@gmail.com>
2023-12-15 15:33:26 +0530

    issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import App from "./App";
 import "./index.css";
 import { Auth0Provider } from '@auth0/auth0-react';
 
-// const onRedirectCallback = (appState) => {
-//   history.push(
-//     appState && appState.returnTo ? appState.returnTo : window.location.pathname
-//   );
-// };
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,21 +13,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain="dev-4jf7lwtlqisur13h.us.auth0.com"
     clientId="fxm1RMrcnIY0u3vyEC6T9zxL2c1nVRLg"
-  
     authorizationParams={{
       redirect_uri: window.location.origin,
      
     }}
-  
     scope='openid email profile'
-    useRefreshTokens={true}
-    additionalSignUpFields={[
-      {
-        name: "email", // Ask for email
-        storage: "root",
-        // ... other options
-      },
-    ]}
   >
     <App />
     </Auth0Provider>
