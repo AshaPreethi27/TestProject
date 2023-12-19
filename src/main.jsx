6fe848c0d1edc1c618358665b9a4c0b56f45ee15
Pilli Asha Preethi <p.ashapreethi@gmail.com>
2023-12-19 22:20:38 +0530

    authentications

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   <BrowserRouter>
-  <Auth0Provider
+  {/* <Auth0Provider
     domain="dev-4jf7lwtlqisur13h.us.auth0.com"
     clientId="fxm1RMrcnIY0u3vyEC6T9zxL2c1nVRLg"
     authorizationParams={{
@@ -18,9 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      
     }}
     scope='openid email profile'
-  >
+  > */}
     <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </BrowserRouter>
   </React.StrictMode>
 );
